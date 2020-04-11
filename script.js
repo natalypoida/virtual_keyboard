90a12ec39d250815568e20ab2313c12bd15161d5
Natalya POida <nataly.poida@gmail.com>
2020-04-11 13:11:47 +0600

    feat:keep coding

--- script.js
+++ script.js
@@ -1,34 +1,66 @@
 const buttons = [
     
 
-]
+];
 
 const virtKeyboard = {
     elements: {
-        wrapper: null,
-        textarea: null,
-        keyboard: null,
-        keys: []
+       main: null,
+       textarea: null,
+       keyboardKeys: null,
     },
 
     properties: {
-        isCapsLock = false,
-        isShift = false,
-        value = ''
-        
+        capsLock: false,
+        value: '',
+        ctrlShift: false
     },
+    
 
     eventHandlers: {
         oninput = null,
-        onclick = null
+        onclick = null,
+        onkeypress = null
+    },
+    
+    init() {
+        this.elements.main =  document.createElement("div");
+        this.elements.textarea = document.createElement("textarea");
+        this.elements.keyboardKeys = document.createElement("div");
+
+        this.elements.main.classList.add("wrapper");
+        this.elements.textarea.classList.add("textarea");
+        this.elements.keyboardKeys.classList.add("keyboard__keys");
+
+        this.elements.main.appendChild(this.elements.textarea, this.elements.keyboardKeys);
+        document.body.appendChild(this.elements.main);
+
+
+
     },
 
-    init() {
-        this.elements.wrapper =  document.createElement('div');
-        this.elements.textarea = document.createElement('textarea');
-        this.elements.keyboard = document.createElement('div');
+    _createKeys() {
 
-        
-    }
-    
-}
+    },
+
+    _eventOn(handlerName) {
+        console.log ("Event is on! Event Name:" + handlerName);
+
+    },
+
+    _capsLockOn() {
+        console.log("CapsLock is On!");
+
+    },
+
+    _ctrlShiftOn() {
+        console.log("Ctrl+Shift is on!");
+
+    },
+
+};
+  
+window.addEventListener("DOMContentLoaded", function() {
+ virtKeyboard.init();
+
+});
